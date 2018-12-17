bb1dc2ab70ecaabe8e2da3e60f9137205e8c38e9
CorwinVoss <mason.cvoss.2019@gmail.com>
2018-12-17 16:36:19 -0500

    Finished Assignment
    git push

--- ProductData/src/com/company/Main.java
+++ ProductData/src/com/company/Main.java
@@ -2,14 +2,35 @@ package com.company;
 
 import com.company.Product;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         String productString = "java:Murach's Java Programming:57.50";
-
+        ArrayList<Character> productCode = new ArrayList<Character>();
+        ArrayList<Character> productDesc = new ArrayList<Character>();
+        ArrayList<Character> productPrice = new ArrayList<Character>();
+        ArrayList<Character> productCharArray = new ArrayList<Character>();
         Product product = new Product();
-
-        System.out.println(productString);
+        for (int i = 0; i < productString.length(); i++){
+            productCharArray.add(productString.charAt(i));
+        }
+        for (int i = 0; productCharArray.get(i) != ':'; i++) {
+            productCode.add(productCharArray.get(i));
+        }
+        for (int i = productCode.size() + 1; productCharArray.get(i) != ':'; i++){
+            productDesc.add(productCharArray.get(i));
+        }
+        for (int i = productCode.size() + productDesc.size() + 2; i != productCharArray.size(); i++){
+            productPrice.add(productCharArray.get(i));
+        }
+        String productCodeString = productCode.toString().replaceAll(", ", "").replaceAll("]", "").replaceAll("\\[", "");
+        product.setCode(productCodeString);
+        String productDescString = productDesc.toString().replaceAll(", ", "").replaceAll("]", "").replaceAll("\\[", "");
+        product.setDescription(productDescString);
+        double price = Double.parseDouble(productPrice.toString().replaceAll(", ", "").replaceAll("]", "").replaceAll("\\[", ""));
+        product.setPrice(price);
 
         //TODO: process productString and populate fields of product object
         
